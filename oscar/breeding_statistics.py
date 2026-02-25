36e0dbcfde6bdc13112a6c94a1819f11bdcfe63e
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-02-25 16:52:14 +0000

    add calculation of mendelian ratio for single mutation scenarios

--- oscar/breeding_statistics.py
+++ oscar/breeding_statistics.py
@@ -1,16 +1,92 @@
 import itertools
-from enum import StrEnum, auto
+from enum import Enum
+
+import numpy as np
 
 
-class Genotype(StrEnum):
-    WT = "wildtype"
-    HOM = "homozygous"
-    HET = "heterozygous"
-    UNGENOTYPED = auto()
+class Genotype(Enum):
+    """Genotype status: homozygous (HOM), heterozygous (HET) or WT (wildtype).
+
+    Each mouse will have two copies a particular gene - each being either
+    wildtype or mutated. The value of the enum is the number of wildtype copies
+    for that genotype.
+    """
+
+    HOM = 0
+    HET = 1
+    WT = 2
+
+
+class BreedingScheme:
+    def __init__(
+        self,
+        parent_1_genotype: tuple[Genotype, ...],
+        parent_2_genotype: tuple[Genotype, ...],
+    ):
+        if len(parent_1_genotype) != len(parent_2_genotype):
+            raise ValueError(
+                "Both parents must have a genotype of the same length"
+            )
+
+        self.parent_1_genotype = parent_1_genotype
+        self.parent_2_genotype = parent_2_genotype
+        self.n_mutations = len(self.parent_1_genotype)
+
+    def __eq__(self, other):
+        # The order of parent 1 vs parent 2 doesn't matter. Breeding
+        # schemes are equal if they are combining the same two genotypes
+        # in any order.
+        return sorted(
+            [self.parent_1_genotype, self.parent_2_genotype]
+        ) == sorted([other.parent_1_genotype, other.parent_2_genotype])
+
+    def mendelian_ratio(self) -> dict[Genotype, float]:
+        """Calculate the theoretical mendelian ratio for this breeding scheme.
+
+        Returns
+        -------
+        dict[tuple[Genotype, ...], float]
+            Returns a dictionary with keys being the possible genotypes of
+            offspring, and values the expected proportion of offspring of
+            that genotype.
+        """
+
+        # first gene
+        parent_1 = self.parent_1_genotype[0]
+        parent_2 = self.parent_2_genotype[0]
+
+        # Each parent has 2 copies of that gene. For wt = both true. For
+        # het = one true, one false. For hom = both false.
+        alleles = []
+        for parent in [parent_1, parent_2]:
+            parent_alleles = [True] * parent.value
+            parent_alleles.extend([False] * (2 - parent.value))
+            alleles.append(parent_alleles)
+
+        offspring_combos = itertools.product(alleles[0], alleles[1])
+
+        total_sum = 0
+        genotype_sums: dict[Genotype, int] = {}
+
+        for combo in offspring_combos:
+            total_sum += 1
+            n_wt_copies = np.array(combo).sum()
+            genotype = Genotype(n_wt_copies)
+
+            if genotype in genotype_sums:
+                genotype_sums[genotype] += 1
+            else:
+                genotype_sums[genotype] = 1
+
+        mendelian_ratios = {}
+        for genotype, n_with_genotype in genotype_sums.items():
+            mendelian_ratios[genotype] = n_with_genotype / total_sum
+
+        return mendelian_ratios
 
 
 def generate_breeding_schemes(
-    number_of_mutations: int,
+    n_mutations: int,
 ) -> list[list[tuple[Genotype, ...]]]:
     """Generate all possible combinations of parent1 x parent2 genotype, for
     the given number of mutations.
@@ -21,20 +97,25 @@ def generate_breeding_schemes(
 
     Parameters
     ----------
-    number_of_mutations : int
+    n_mutations : int
         Number of mutations
+
+    Returns
+    -------
+    list[list[tuple[Genotype, ...]]]
+        A list where each item is a possible breeding scheme. Each item is
+        a list of form: [(parent_1_genotype), (parent_2_genotype)]
     """
 
-    genotypes = [Genotype.WT, Genotype.HOM, Genotype.HET]
     breeding_schemes = []
 
     # First, generate all possible genotypes of a single parent.
-    parent_genotype = itertools.product(genotypes, repeat=number_of_mutations)
+    parent_genotypes = generate_genotypes(n_mutations)
 
     # Then combine two parents, bearing in mind order doesn't matter e.g.
     # wt x hom == hom x wt
     breeding_combos = itertools.combinations_with_replacement(
-        parent_genotype, 2
+        parent_genotypes, 2
     )
 
     for combo in breeding_combos:
@@ -49,6 +130,24 @@ def generate_breeding_schemes(
             breeding_schemes.append([parent_1, parent_2])
 
     return breeding_schemes
+
+
+def generate_genotypes(n_mutations: int) -> itertools.product:
+    """Generate all possible genotypes of a single mouse, with the given
+    number of mutations.
+
+    Parameters
+    ----------
+    n_mutations : int
+        Number of mutations
+
+    Returns
+    -------
+    itertools.product
+        An iterable of all possible genotypes
+    """
+    single_genotypes = [Genotype.WT, Genotype.HOM, Genotype.HET]
+    return itertools.product(single_genotypes, repeat=n_mutations)
 
 
 def _breeding_scheme_contains_wt_pairs(
--- tests/test_unit/test_breeding_statistics.py
+++ tests/test_unit/test_breeding_statistics.py
@@ -1,6 +1,10 @@
 import pytest
 
-from oscar.breeding_statistics import Genotype, generate_breeding_schemes
+from oscar.breeding_statistics import (
+    BreedingScheme,
+    Genotype,
+    generate_breeding_schemes,
+)
 
 
 @pytest.mark.parametrize(
@@ -64,3 +68,50 @@ def test_generate_breeding_schemes(
 ):
     schemes = generate_breeding_schemes(n_mutations)
     assert schemes == expected_schemes
+
+
+@pytest.mark.parametrize(
+    "parent_1_genotype, parent_2_genotype, expected_ratios",
+    [
+        pytest.param(
+            (Genotype.WT,),
+            (Genotype.HET,),
+            {Genotype.WT: 0.5, Genotype.HET: 0.5},
+            id="WT x HET",
+        ),
+        pytest.param(
+            (Genotype.WT,),
+            (Genotype.HOM,),
+            {Genotype.HET: 1},
+            id="WT x HOM",
+        ),
+        pytest.param(
+            (Genotype.HET,),
+            (Genotype.HOM,),
+            {Genotype.HOM: 0.5, Genotype.HET: 0.5},
+            id="HET x HOM",
+        ),
+        pytest.param(
+            (Genotype.HOM,),
+            (Genotype.HOM,),
+            {
+                Genotype.HOM: 1,
+            },
+            id="HOM x HOM",
+        ),
+        pytest.param(
+            (Genotype.HET,),
+            (Genotype.HET,),
+            {Genotype.WT: 0.25, Genotype.HET: 0.5, Genotype.HOM: 0.25},
+            id="HET x HET",
+        ),
+    ],
+)
+def test_mendelian_ratio_single_mutation(
+    parent_1_genotype, parent_2_genotype, expected_ratios
+):
+    scheme = BreedingScheme(
+        parent_1_genotype=parent_1_genotype,
+        parent_2_genotype=parent_2_genotype,
+    )
+    assert scheme.mendelian_ratio() == expected_ratios
