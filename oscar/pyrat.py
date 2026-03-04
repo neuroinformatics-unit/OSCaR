c818f1dc2a20f9ffcfea63c9a6277196c802c859
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-03-04 15:09:01 +0000

    draft of combined mutation and grade columns for all ids

--- oscar/pyrat.py
+++ oscar/pyrat.py
@@ -2,42 +2,42 @@ from pathlib import Path
 
 import pandas as pd
 
+from oscar.breeding_scheme import Genotype
+
 
 def standardise_pyrat_csv(input_csv: pd.DataFrame | Path) -> pd.DataFrame:
     if isinstance(input_csv, Path):
         input_csv = pd.read_csv(input_csv)
 
-    # columns of form 'Grade NUMBER'
-    offspring_genotype_cols = list(
-        input_csv.columns[input_csv.columns.str.contains(r"^Grade \d$")]
+    offspring_mutation_cols, offspring_genotype_cols = (
+        _get_mutation_and_genotype_columns(input_csv)
     )
-
-    # columns of form 'Father: Grade NUMBER'
-    father_genotype_cols = list(
-        input_csv.columns[
-            input_csv.columns.str.contains(r"^Father: Grade \d$")
-        ]
+    father_mutation_cols, father_genotype_cols = (
+        _get_mutation_and_genotype_columns(input_csv, prefix="Father: ")
     )
-
-    # columns of form 'Mother: Grade NUMBER'
-    mother_genotype_cols = list(
-        input_csv.columns[
-            input_csv.columns.str.contains(r"^Mother: Grade \d$")
-        ]
+    mother_mutation_cols, mother_genotype_cols = (
+        _get_mutation_and_genotype_columns(input_csv, prefix="Mother: ")
     )
 
     all_genotype_cols = (
         offspring_genotype_cols + father_genotype_cols + mother_genotype_cols
     )
+    all_mutation_cols = (
+        offspring_mutation_cols + father_mutation_cols + mother_mutation_cols
+    )
 
-    required_cols = [
-        "ID",
-        "Line / Strain (Name)",
-        "DOB",
-        "Father",
-        "Mother",
-        "Sacrifice reason",
-    ] + all_genotype_cols
+    required_cols = (
+        [
+            "ID",
+            "Line / Strain (Name)",
+            "DOB",
+            "Father",
+            "Mother",
+            "Sacrifice reason",
+        ]
+        + all_mutation_cols
+        + all_genotype_cols
+    )
 
     # Get rid of any additional columns + rename to standard names
     standard_csv = input_csv[required_cols]
@@ -55,10 +55,9 @@ def standardise_pyrat_csv(input_csv: pd.DataFrame | Path) -> pd.DataFrame:
     standard_csv = _filter_allowed_genotypes(standard_csv, all_genotype_cols)
     standard_csv = _filter_ungenotyped(standard_csv, offspring_genotype_cols)
 
-    n_mutations = 0
-    for col_name in offspring_genotype_cols:
-        n_mutations += standard_csv[col_name].notna().any()
-    standard_csv["n_mutations"] = n_mutations
+    standard_csv["n_mutations"] = (
+        standard_csv.loc[:, offspring_genotype_cols].notna().sum(axis=1)
+    )
 
     # make sure number of mutations is the same throughout each line -
     # use the max.
@@ -67,6 +66,8 @@ def standardise_pyrat_csv(input_csv: pd.DataFrame | Path) -> pd.DataFrame:
     standard_csv["n_mutations"] = standard_csv.groupby("line_name")[
         "n_mutations"
     ].transform("max")
+
+    _make_combined_genotype_columns(standard_csv)
 
     # Combine grade 1/2/3 columns into an overall genotype
     _make_combined_genotype_column(
@@ -86,6 +87,26 @@ def standardise_pyrat_csv(input_csv: pd.DataFrame | Path) -> pd.DataFrame:
     return standard_csv
 
 
+def _get_mutation_and_genotype_columns(
+    input_csv: pd.DataFrame, prefix: str = ""
+) -> tuple[list[str], list[str]]:
+    # columns of form 'PREFIXMutation NUMBER'
+    mutation_cols = list(
+        input_csv.columns[
+            input_csv.columns.str.contains(rf"^{prefix}Mutation \d$")
+        ]
+    )
+
+    # columns of form 'PREFIXGrade NUMBER'
+    genotype_cols = list(
+        input_csv.columns[
+            input_csv.columns.str.contains(rf"^{prefix}Grade \d$")
+        ]
+    )
+
+    return mutation_cols, genotype_cols
+
+
 def _filter_allowed_genotypes(
     standard_csv: pd.DataFrame, genotype_cols: list[str]
 ) -> pd.DataFrame:
@@ -102,7 +123,8 @@ def _filter_allowed_genotypes(
 
     genotype_data = standard_csv.loc[:, genotype_cols]
     allowed_genotypes = (
-        genotype_data.isin(["wt", "het", "hom"]) | genotype_data.isna()
+        genotype_data.isin([genotype.name.lower() for genotype in Genotype])
+        | genotype_data.isna()
     ).all(axis=1)
     filtered_data = standard_csv.loc[allowed_genotypes, :]
 
@@ -136,6 +158,55 @@ def _filter_ungenotyped(
     return filtered_data
 
 
+def _make_combined_genotype_columns(standard_csv: pd.DataFrame):
+    # Combine columns for mutation/grade positions into one column.
+    # i.e. combine Mutation 1/2/3 -> Mutation; Father: Grade 1/2/3 into
+    # Father: Grade and so on...
+    long_format = pd.wide_to_long(
+        standard_csv,
+        stubnames=[
+            "Mutation",
+            "Father: Mutation",
+            "Mother: Mutation",
+            "Grade",
+            "Father: Grade",
+            "Mother: Grade",
+        ],
+        i="ID_offspring",
+        j="mutation_number",
+        sep=" ",
+    ).reset_index()
+
+    # Rename columns so that the individual the mutation / grade belongs to
+    # (offspring, father or mother) is the suffix
+    long_format = long_format.rename(
+        columns={
+            "Mutation": "Mutation_offspring",
+            "Grade": "Grade_offspring",
+            "Father: Mutation": "Mutation_father",
+            "Father: Grade": "Grade_father",
+            "Mother: Mutation": "Mutation_mother",
+            "Mother: Grade": "Grade_mother",
+        }
+    )
+
+    # Combine offspring / father / mother columns into one column.
+    # i.e. Mutation_offspring/father/mother -> Mutation;
+    # Grade_offspring/father/mother -> Grade
+    long_format = (
+        pd.wide_to_long(
+            long_format,
+            stubnames=["Mutation", "Grade"],
+            i=["ID_offspring", "mutation_number"],
+            j="mutation_belongs_to",
+            sep="_",
+            suffix=r"\w+",
+        )
+        .reset_index()
+        .sort_values("ID_offspring")
+    )
+
+
 def _make_combined_genotype_column(
     standard_csv: pd.DataFrame, cols_to_combine: list[str], new_col_name: str
 ) -> None:
@@ -167,3 +238,13 @@ def _make_combined_genotype_column(
                 + "_"
                 + standard_csv[col_name].fillna("wt")
             )
+
+
+def _make_combined_genotype_column_for_line(
+    line_data: pd.DataFrame, mutation_cols: list[str], genotype_cols: list[str]
+):
+    # convert dataframe to 'long' format i.e. combine all mutation cols into
+    # one, and all genotype cols into one. Each ID_offspring will have multiple
+    # rows - one per mutation.
+    print(line_data)
+    print(line_data.columns)
