d01b5edb1091dce8a5b71e961c088e83407781ce
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-03-09 13:47:06 +0000

    add test for 3 mutations line statistics

--- tests/test_unit/test_historical_stats.py
+++ tests/test_unit/test_historical_stats.py
@@ -193,6 +193,130 @@ def expected_stats_2_mutations():
     )
 
 
+@pytest.fixture
+def expected_stats_3_mutations():
+    return LineStatistics(
+        total_n_offspring=20,
+        total_n_offspring_per_genotype={
+            (Genotype.HET, Genotype.WT, Genotype.HOM): 8,
+            (Genotype.WT, Genotype.HET, Genotype.HOM): 2,
+            (Genotype.HOM, Genotype.WT, Genotype.HET): 2,
+            (Genotype.WT, Genotype.WT, Genotype.HET): 1,
+            (Genotype.HET, Genotype.WT, Genotype.WT): 1,
+            (Genotype.WT, Genotype.HET, Genotype.WT): 3,
+            (Genotype.HET, Genotype.HET, Genotype.WT): 2,
+            (Genotype.WT, Genotype.HET, Genotype.HET): 1,
+        },
+        stats_per_breeding_scheme={
+            BreedingScheme(
+                "wt_wt_het", "het_het_het"
+            ): BreedingSchemeStatistics(
+                n_breeding_pairs=2,
+                n_successful_matings=3,
+                average_litter_size=pytest.approx(2.666, abs=1e-3),
+                average_n_litters_per_pair=pytest.approx(1.5, abs=1e-3),
+                total_n_offspring=8,
+                n_offspring_per_genotype={
+                    (Genotype.HET, Genotype.WT, Genotype.HOM): 4,
+                    (Genotype.WT, Genotype.HET, Genotype.HOM): 2,
+                    (Genotype.HOM, Genotype.WT, Genotype.HET): 2,
+                },
+                proportion_offspring_per_genotype={
+                    (Genotype.HET, Genotype.WT, Genotype.HOM): pytest.approx(
+                        0.5, abs=1e-3
+                    ),
+                    (Genotype.WT, Genotype.HET, Genotype.HOM): pytest.approx(
+                        0.25, abs=1e-3
+                    ),
+                    (Genotype.HOM, Genotype.WT, Genotype.HET): pytest.approx(
+                        0.25, abs=1e-3
+                    ),
+                },
+            ),
+            BreedingScheme(
+                "wt_het_het", "het_het_hom"
+            ): BreedingSchemeStatistics(
+                n_breeding_pairs=2,
+                n_successful_matings=2,
+                average_litter_size=pytest.approx(2.0, abs=1e-3),
+                average_n_litters_per_pair=pytest.approx(1.0, abs=1e-3),
+                total_n_offspring=4,
+                n_offspring_per_genotype={
+                    (Genotype.HET, Genotype.WT, Genotype.HOM): 4
+                },
+                proportion_offspring_per_genotype={
+                    (Genotype.HET, Genotype.WT, Genotype.HOM): pytest.approx(
+                        1.0, abs=1e-3
+                    )
+                },
+            ),
+            BreedingScheme(
+                "het_wt_wt", "wt_het_hom"
+            ): BreedingSchemeStatistics(
+                n_breeding_pairs=2,
+                n_successful_matings=2,
+                average_litter_size=pytest.approx(1.5, abs=1e-3),
+                average_n_litters_per_pair=pytest.approx(1.0, abs=1e-3),
+                total_n_offspring=3,
+                n_offspring_per_genotype={
+                    (Genotype.WT, Genotype.WT, Genotype.HET): 1,
+                    (Genotype.HET, Genotype.WT, Genotype.WT): 1,
+                    (Genotype.WT, Genotype.HET, Genotype.WT): 1,
+                },
+                proportion_offspring_per_genotype={
+                    (Genotype.WT, Genotype.WT, Genotype.HET): pytest.approx(
+                        0.333, abs=1e-3
+                    ),
+                    (Genotype.HET, Genotype.WT, Genotype.WT): pytest.approx(
+                        0.333, abs=1e-3
+                    ),
+                    (Genotype.WT, Genotype.HET, Genotype.WT): pytest.approx(
+                        0.333, abs=1e-3
+                    ),
+                },
+            ),
+            BreedingScheme(
+                "het_wt_wt", "wt_hom_het"
+            ): BreedingSchemeStatistics(
+                n_breeding_pairs=2,
+                n_successful_matings=2,
+                average_litter_size=pytest.approx(1.0, abs=1e-3),
+                average_n_litters_per_pair=pytest.approx(1.0, abs=1e-3),
+                total_n_offspring=2,
+                n_offspring_per_genotype={
+                    (Genotype.WT, Genotype.HET, Genotype.WT): 2,
+                },
+                proportion_offspring_per_genotype={
+                    (Genotype.WT, Genotype.HET, Genotype.WT): pytest.approx(
+                        1.0, abs=1e-3
+                    ),
+                },
+            ),
+            BreedingScheme(
+                "wt_hom_het", "hom_wt_wt"
+            ): BreedingSchemeStatistics(
+                n_breeding_pairs=1,
+                n_successful_matings=1,
+                average_litter_size=pytest.approx(3.0, abs=1e-3),
+                average_n_litters_per_pair=pytest.approx(1.0, abs=1e-3),
+                total_n_offspring=3,
+                n_offspring_per_genotype={
+                    (Genotype.HET, Genotype.HET, Genotype.WT): 2,
+                    (Genotype.WT, Genotype.HET, Genotype.HET): 1,
+                },
+                proportion_offspring_per_genotype={
+                    (Genotype.HET, Genotype.HET, Genotype.WT): pytest.approx(
+                        0.666, abs=1e-3
+                    ),
+                    (Genotype.WT, Genotype.HET, Genotype.HET): pytest.approx(
+                        0.333, abs=1e-3
+                    ),
+                },
+            ),
+        },
+    )
+
+
 @pytest.mark.parametrize(
     "standardised_csv_path, line_name, expected_stats",
     [
@@ -207,6 +331,12 @@ def expected_stats_2_mutations():
             "Line-AB",
             "expected_stats_2_mutations",
             id="2 mutations",
+        ),
+        pytest.param(
+            "standardised_3_mutations_csv_path",
+            "Line-ABC",
+            "expected_stats_3_mutations",
+            id="3 mutations",
         ),
     ],
 )
