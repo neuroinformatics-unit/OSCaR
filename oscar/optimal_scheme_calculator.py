f357c3871f9441809616aa1b6a39e59ad1e77e4a
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-03-10 14:56:08 +0000

    calculate summary of surplus for optimal scheme and force integer values for result

--- oscar/optimal_scheme_calculator.py
+++ oscar/optimal_scheme_calculator.py
@@ -1,3 +1,6 @@
+import math
+from dataclasses import dataclass, field
+
 from scipy.optimize import linprog
 
 from oscar.breeding_scheme import (
@@ -6,6 +9,22 @@ from oscar.breeding_scheme import (
     generate_breeding_schemes,
 )
 from oscar.historical_stats import LineStatistics
+
+
+@dataclass
+class SurplusSummary:
+    total_n: int = 0
+    total_n_surplus: int = 0
+
+    total_n_per_genotype: dict[tuple[Genotype, ...], int] = field(
+        default_factory=dict
+    )
+    total_surplus_per_genotype: dict[tuple[Genotype, ...], int] = field(
+        default_factory=dict
+    )
+    percent_surplus_per_genotype: dict[tuple[Genotype, ...], float] = field(
+        default_factory=dict
+    )
 
 
 def calculate_optimal_scheme(
@@ -40,7 +59,7 @@ def _optimise_n_matings(
     n_per_scheme_genotype: dict[
         BreedingScheme, dict[tuple[Genotype, ...], float]
     ],
-):
+) -> dict[BreedingScheme, int]:
     """Calculate the optimal number of matings of each breeding scheme to
     meet the required_genotypes.
 
@@ -59,6 +78,11 @@ def _optimise_n_matings(
         dict[BreedingScheme, dict[tuple[Genotype, ...], float]]
         _description_
     """
+
+    # Extract names of breeding schemes / required genotypes as a list, so we
+    # can make sure we always iterate through them in the same order
+    breeding_schemes = list(litter_size_per_scheme.keys())
+    required_genotypes = list(required_n_per_genotype.keys())
 
     # Coefficients of the function defining the amount of surplus (we want to
     # minimise).
@@ -84,18 +108,14 @@ def _optimise_n_matings(
     constraint_coefficients: list[list[float]] = []
     constraint_constants = []
 
-    # Extract names of required genotypes as list, so we can make sure
-    # we always iterate through them in the same order for each breeding
-    # scheme
-    required_genotypes = list(required_n_per_genotype.keys())
     for genotype in required_genotypes:
         constraint_coefficients.append([])
         # Multiply by -1 as linprog only accepts <= constraints, and ours
         # is >=
         constraint_constants.append(required_n_per_genotype[genotype] * -1)
 
-    for breeding_scheme, litter_size in litter_size_per_scheme.items():
-        objective_coefficients.append(litter_size)
+    for breeding_scheme in breeding_schemes:
+        objective_coefficients.append(litter_size_per_scheme[breeding_scheme])
         n_per_genotype = n_per_scheme_genotype[breeding_scheme]
 
         for i, genotype in enumerate(required_genotypes):
@@ -110,14 +130,62 @@ def _optimise_n_matings(
     # By default linprog has bounds of 0-infinity for variables (i.e. the
     # n_matings) so no need to specify further here.
 
-    linprog(
+    optimised_result = linprog(
         objective_coefficients,
         A_ub=constraint_coefficients,
         b_ub=constraint_constants,
+        integrality=1,  # returned values for n of matings must be integers
         method="highs",
     )
 
-    pass
+    if (not optimised_result.status == 0) or (not optimised_result.success):
+        raise ValueError(
+            f"Number of matings couldn't be optimised: "
+            f"{optimised_result.message}"
+        )
+
+    n_matings_per_scheme = dict(zip(breeding_schemes, optimised_result.x))
+
+    return n_matings_per_scheme
+
+
+def _create_surplus_summary(
+    required_n_per_genotype: dict[tuple[Genotype, ...], int],
+    n_matings_per_scheme: dict[BreedingScheme, int],
+    n_per_scheme_genotype,
+):
+    surplus_summary = SurplusSummary()
+
+    for breeding_scheme, n_matings in n_matings_per_scheme.items():
+        n_per_genotype = n_per_scheme_genotype[breeding_scheme]
+
+        total_for_scheme = 0
+        for genotype, n in n_per_genotype.items():
+            # round value up to the nearest int - as we can't have
+            # partial animals
+            total_n = math.ceil(n * n_matings)
+            total_for_scheme += total_n
+
+            if genotype in surplus_summary.total_n_per_genotype:
+                surplus_summary.total_n_per_genotype[genotype] += total_n
+            else:
+                surplus_summary.total_n_per_genotype[genotype] = total_n
+
+        surplus_summary.total_n += total_for_scheme
+
+    total_required = sum(
+        [required_n for required_n in required_n_per_genotype.values()]
+    )
+    surplus_summary.total_n_surplus = surplus_summary.total_n - total_required
+
+    for genotype, total_n in surplus_summary.total_n_per_genotype.items():
+        total_surplus = total_n - required_n_per_genotype[genotype]
+        surplus_summary.total_surplus_per_genotype[genotype] = total_surplus
+        surplus_summary.percent_surplus_per_genotype[genotype] = (
+            total_surplus / surplus_summary.total_n_per_genotype[genotype]
+        ) * 100
+
+    return surplus_summary
 
 
 def _get_estimated_litter_sizes(breeding_schemes, line_stats, min_n_matings):
