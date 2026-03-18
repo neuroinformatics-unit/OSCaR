2532a730cb8a8dad05fee87b5e827050333c3014
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-03-18 09:17:14 +0000

    expand estimate_n_offspring test

--- tests/test_unit/test_optimal_scheme_calculator.py
+++ tests/test_unit/test_optimal_scheme_calculator.py
@@ -14,82 +14,170 @@ from oscar.optimal_scheme_calculator import (
 @pytest.fixture
 def example_line_stats():
     return LineStatistics(
-        total_n_offspring=100,
+        total_n_offspring=140,
         total_n_offspring_per_genotype={
-            (Genotype.WT,): 70,
+            (Genotype.WT,): 110,
             (Genotype.HET,): 24,
             (Genotype.HOM,): 6,
         },
+        total_n_successful_matings=14,
+        average_litter_size=10,
         stats_per_breeding_scheme={
             BreedingScheme("wt", "het"): BreedingSchemeStatistics(
                 n_breeding_pairs=3,
                 n_successful_matings=9,
-                average_litter_size=6,
+                average_litter_size=8,
                 average_n_litters_per_pair=3,
-                total_n_offspring=54,
+                total_n_offspring=72,
                 n_offspring_per_genotype={
-                    (Genotype.WT,): 40,
+                    (Genotype.WT,): 58,
                     (Genotype.HET,): 14,
                 },
                 proportion_offspring_per_genotype={
-                    (Genotype.WT,): 40 / 54,
-                    (Genotype.HET,): 14 / 54,
+                    (Genotype.WT,): 58 / 72,
+                    (Genotype.HET,): 14 / 72,
                 },
             ),
             BreedingScheme("het", "het"): BreedingSchemeStatistics(
                 n_breeding_pairs=2,
                 n_successful_matings=5,
-                average_litter_size=46 / 5,
+                average_litter_size=13.6,
                 average_n_litters_per_pair=2.5,
-                total_n_offspring=46,
+                total_n_offspring=68,
                 n_offspring_per_genotype={
-                    (Genotype.WT,): 30,
+                    (Genotype.WT,): 52,
                     (Genotype.HET,): 10,
                     (Genotype.HOM,): 6,
                 },
                 proportion_offspring_per_genotype={
-                    (Genotype.WT,): 30 / 46,
-                    (Genotype.HET,): 10 / 46,
-                    (Genotype.HOM,): 6 / 46,
+                    (Genotype.WT,): 52 / 68,
+                    (Genotype.HET,): 10 / 68,
+                    (Genotype.HOM,): 6 / 68,
                 },
             ),
         },
     )
 
 
-def test_estimate_n_offspring_per_mating(example_line_stats):
+@pytest.mark.parametrize(
+    (
+        "min_n_offspring, min_n_matings, default_litter_size, "
+        "expected_offspring_per_scheme"
+    ),
+    [
+        pytest.param(
+            1,
+            1,
+            4,
+            {
+                BreedingScheme("wt", "hom"): ExpectedOffspring(
+                    total_n=10, n_per_genotype={(Genotype.HET,): 10.0}
+                ),
+                BreedingScheme("wt", "het"): ExpectedOffspring(
+                    total_n=8,
+                    n_per_genotype={
+                        (Genotype.WT,): pytest.approx((58 / 72) * 8),
+                        (Genotype.HET,): pytest.approx((14 / 72) * 8),
+                    },
+                ),
+                BreedingScheme("hom", "hom"): ExpectedOffspring(
+                    total_n=10, n_per_genotype={(Genotype.HOM,): 10.0}
+                ),
+                BreedingScheme("hom", "het"): ExpectedOffspring(
+                    total_n=10,
+                    n_per_genotype={(Genotype.HOM,): 5, (Genotype.HET,): 5},
+                ),
+                BreedingScheme("het", "het"): ExpectedOffspring(
+                    total_n=13.6,
+                    n_per_genotype={
+                        (Genotype.WT,): pytest.approx((52 / 68) * 13.6),
+                        (Genotype.HET,): pytest.approx((10 / 68) * 13.6),
+                        (Genotype.HOM,): pytest.approx((6 / 68) * 13.6),
+                    },
+                ),
+            },
+            id="Min values == 1",
+        ),
+        pytest.param(
+            70,
+            0,
+            4,
+            {
+                BreedingScheme("wt", "hom"): ExpectedOffspring(
+                    total_n=10, n_per_genotype={(Genotype.HET,): 10.0}
+                ),
+                BreedingScheme("wt", "het"): ExpectedOffspring(
+                    total_n=8,
+                    n_per_genotype={
+                        (Genotype.WT,): pytest.approx((58 / 72) * 8),
+                        (Genotype.HET,): pytest.approx((14 / 72) * 8),
+                    },
+                ),
+                BreedingScheme("hom", "hom"): ExpectedOffspring(
+                    total_n=10, n_per_genotype={(Genotype.HOM,): 10.0}
+                ),
+                BreedingScheme("hom", "het"): ExpectedOffspring(
+                    total_n=10,
+                    n_per_genotype={(Genotype.HOM,): 5, (Genotype.HET,): 5},
+                ),
+                BreedingScheme("het", "het"): ExpectedOffspring(
+                    total_n=13.6,
+                    n_per_genotype={
+                        (Genotype.WT,): pytest.approx(0.25 * 13.6),
+                        (Genotype.HET,): pytest.approx(0.5 * 13.6),
+                        (Genotype.HOM,): pytest.approx(0.25 * 13.6),
+                    },
+                ),
+            },
+            id="min_n_offspring=70. hetxhet should use mendelian ratios now",
+        ),
+        pytest.param(
+            0,
+            6,
+            4,
+            {
+                BreedingScheme("wt", "hom"): ExpectedOffspring(
+                    total_n=10, n_per_genotype={(Genotype.HET,): 10.0}
+                ),
+                BreedingScheme("wt", "het"): ExpectedOffspring(
+                    total_n=8,
+                    n_per_genotype={
+                        (Genotype.WT,): pytest.approx((58 / 72) * 8),
+                        (Genotype.HET,): pytest.approx((14 / 72) * 8),
+                    },
+                ),
+                BreedingScheme("hom", "hom"): ExpectedOffspring(
+                    total_n=10, n_per_genotype={(Genotype.HOM,): 10.0}
+                ),
+                BreedingScheme("hom", "het"): ExpectedOffspring(
+                    total_n=10,
+                    n_per_genotype={(Genotype.HOM,): 5, (Genotype.HET,): 5},
+                ),
+                BreedingScheme("het", "het"): ExpectedOffspring(
+                    total_n=10.0,
+                    n_per_genotype={
+                        (Genotype.WT,): pytest.approx((52 / 68) * 10.0),
+                        (Genotype.HET,): pytest.approx((10 / 68) * 10.0),
+                        (Genotype.HOM,): pytest.approx((6 / 68) * 10.0),
+                    },
+                ),
+            },
+            id="min_n_matings=7. hetxhet should use default_litter_size now",
+        ),
+    ],
+)
+def test_estimate_n_offspring_per_mating(
+    example_line_stats,
+    min_n_offspring,
+    min_n_matings,
+    default_litter_size,
+    expected_offspring_per_scheme,
+):
     offspring_per_scheme = _estimate_n_offspring_per_mating(
         example_line_stats,
-        min_n_offspring=0,
-        min_n_matings=0,
-        default_litter_size=4,
+        min_n_offspring=min_n_offspring,
+        min_n_matings=min_n_matings,
+        default_litter_size=default_litter_size,
     )
-
-    expected_offspring_per_scheme = {
-        BreedingScheme("wt", "hom"): ExpectedOffspring(
-            total_n=0, n_per_genotype={}
-        ),
-        BreedingScheme("wt", "het"): ExpectedOffspring(
-            total_n=6,
-            n_per_genotype={
-                (Genotype.WT,): pytest.approx((40 / 54) * 6),
-                (Genotype.HET,): pytest.approx((14 / 54) * 6),
-            },
-        ),
-        BreedingScheme("hom", "hom"): ExpectedOffspring(
-            total_n=0, n_per_genotype={}
-        ),
-        BreedingScheme("hom", "het"): ExpectedOffspring(
-            total_n=0, n_per_genotype={}
-        ),
-        BreedingScheme("het", "het"): ExpectedOffspring(
-            total_n=9.2,
-            n_per_genotype={
-                (Genotype.WT,): pytest.approx((30 / 46) * 9.2),
-                (Genotype.HET,): pytest.approx((10 / 46) * 9.2),
-                (Genotype.HOM,): pytest.approx((6 / 46) * 9.2),
-            },
-        ),
-    }
 
     assert offspring_per_scheme == expected_offspring_per_scheme
