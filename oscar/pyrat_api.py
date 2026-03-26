477dcc87290930c83caa0f0efdc5c778cc6da45a
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-03-26 11:51:56 +0000

    change payload to params and expand docstrings

--- oscar/pyrat_api.py
+++ oscar/pyrat_api.py
@@ -16,7 +16,7 @@ def get_pyrat_data(
     ):
         raise ValueError("birth_date_to must be after birth_date_from")
 
-    payload = {
+    params = {
         "k": [
             "animalid",
             "eartag_or_id",
@@ -33,30 +33,44 @@ def get_pyrat_data(
     }
 
     if line_name is not None:
-        payload["strain_name_with_id_like"] = line_name
+        params["strain_name_with_id_like"] = line_name
 
     if species_name is not None:
-        payload["species"] = _get_species_id(species_name)
+        params["species"] = _get_species_id(species_name)
 
     if birth_date_from is not None:
-        payload["birth_date_from"] = birth_date_from.isoformat()
+        params["birth_date_from"] = birth_date_from.isoformat()
 
     if birth_date_to is not None:
-        payload["birth_date_to"] = birth_date_to.isoformat()
+        params["birth_date_to"] = birth_date_to.isoformat()
 
-    animals = _make_pyrat_request("animals", payload)
+    animals = _make_pyrat_request("animals", params)
 
     return animals
 
 
-def _make_pyrat_request(endpoint_name: str, payload: dict[str, Any]) -> Any:
+def _make_pyrat_request(endpoint_name: str, params: dict[str, Any]) -> Any:
+    """Make request to the pyRAT api.
+
+    Parameters
+    ----------
+    endpoint_name : str
+        Name of endpoint e.g. 'species'
+    params : dict[str, Any]
+        Extra parameters to pass to the endpoint
+
+    Returns
+    -------
+    Any
+        Response decoded from json into a python object
+    """
     response = requests.get(
         url=f"{os.environ['PYRAT_URL']}/api/v3/{endpoint_name}",
         auth=(
             os.environ["PYRAT_CLIENT_TOKEN"],
             os.environ["PYRAT_USER_TOKEN"],
         ),
-        params=payload,
+        params=params,
         timeout=2,  # number of seconds before timeout
     )
 
@@ -70,11 +84,11 @@ def _make_pyrat_request(endpoint_name: str, payload: dict[str, Any]) -> Any:
 def _get_species_id(species_name: str) -> int:
     """Get pyRAT database ID for named species"""
 
-    payload = {
+    params = {
         "k": ["id", "name"],
         "s": ["name:asc"],
     }
-    species_ids = _make_pyrat_request("species", payload)
+    species_ids = _make_pyrat_request("species", params)
 
     available_names = []
     for species in species_ids:
