081c9bf3cad0367de9523358284b6c8dd57212e0
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2026-04-08 17:08:53 +0100

    mock father and mother api responses

--- tests/test_unit/pyrat/test_api.py
+++ tests/test_unit/pyrat/test_api.py
@@ -2,6 +2,7 @@ import datetime
 import os
 
 import responses
+from responses import matchers
 
 from oscar.pyrat.api import get_pyrat_data
 
@@ -9,7 +10,7 @@ from oscar.pyrat.api import get_pyrat_data
 @responses.activate
 def test_get_pyrat_data():
     pyrat_url = os.environ["PYRAT_URL"]
-    line_name = "Line-AB"
+    line_name = "Line-A"
 
     # mock species response
     responses.get(
@@ -17,13 +18,50 @@ def test_get_pyrat_data():
         json=[{"id": 1, "name": "Mouse"}, {"id": 2, "name": "Rat"}],
     )
 
-    # mock animals response
+    # mock mother response
+    params = {"eartag": "ID-100"}
+    responses.get(
+        f"{pyrat_url}/api/v3/animals",
+        match=[matchers.query_param_matcher(params, strict_match=False)],
+        json=[
+            {
+                "animalid": 100,
+                "eartag_or_id": "ID-100",
+                "mutations": [],
+            }
+        ],
+    )
+
+    # mock father response
+    params = {"eartag": "ID-101"}
+    responses.get(
+        f"{pyrat_url}/api/v3/animals",
+        match=[matchers.query_param_matcher(params, strict_match=False)],
+        json=[
+            {
+                "animalid": 101,
+                "eartag_or_id": "ID-101",
+                "mutations": [
+                    {
+                        "animalid": 101,
+                        "mutation_id": 5,
+                        "mutationname": "Mut-A",
+                        "mutationtype": "BAC transgenic",
+                        "grade_id": 5,
+                        "mutationgrade": "het",
+                    }
+                ],
+            },
+        ],
+    )
+
+    # mock offspring response
     responses.get(
         f"{pyrat_url}/api/v3/animals",
         json=[
             {
                 "animalid": 1,
-                "eartag_or_id": "A1",
+                "eartag_or_id": "ID-001",
                 "species_name": "Mouse",
                 "sacrificice_reason_name": "Not usable age",
                 "dateborn": "2026-02-03T00:00:00",
@@ -40,17 +78,17 @@ def test_get_pyrat_data():
                 ],
                 "parents": [
                     {
-                        "animalid": 100,
-                        "parent_id": 1000,
-                        "parent_eartag": "A100",
+                        "animalid": 1,
+                        "parent_id": 100,
+                        "parent_eartag": "ID-100",
                         "parent_sex": "m",
                         "parent_labid": "B",
                         "parent_cagenumber": "B2",
                     },
                     {
-                        "animalid": 101,
-                        "parent_id": 1001,
-                        "parent_eartag": "A101",
+                        "animalid": 1,
+                        "parent_id": 101,
+                        "parent_eartag": "ID-101",
                         "parent_sex": "f",
                         "parent_labid": "B",
                         "parent_cagenumber": "B2",
@@ -58,6 +96,7 @@ def test_get_pyrat_data():
                 ],
             }
         ],
+        headers={"x-count": "1", "x-total-count": "1"},
     )
 
     pyrat_dfs = get_pyrat_data(
@@ -67,3 +106,5 @@ def test_get_pyrat_data():
         birth_date_to=datetime.date(2026, 2, 6),
     )
     pyrat_dfs = list(pyrat_dfs)
+
+    assert len(pyrat_dfs) == 1
